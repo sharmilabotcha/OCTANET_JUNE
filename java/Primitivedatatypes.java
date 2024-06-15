43242fb3def331cbea5c8083b67ada2f4016afc6
sharmilabotcha <105410405+sharmilabotcha@users.noreply.github.com>
2024-06-15 12:22:22 +0530

    Remove all files and folders from repository

--- java/Primitivedatatypes.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class Primitivedatatypes {
-    public static void main(String[] args){
-        int num = 'A';
-        System.out.println(num);
-
-    }
-    
-}
--- java/ex1.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-
-public class ex1 {
-    public static void main(String[] args) {
-        Scanner
-    }
-    
-}
